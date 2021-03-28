ba4d60121ebaba48e6c29fce24f6071497b18341
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2021-03-28 23:24:55 +0530

    add back link from details page to main page

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,15 +1,22 @@
 import Head from "next/head";
-import { PageContainer, PageHeader } from "../components/primitives";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { PageContainer } from "../components/primitives";
 
 export default function RecipeDetails() {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <PageContainer>
       <Head>
         <title>Recipe Detail</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <PageHeader title="Recipe Detail" />
-      <h2>Details page </h2>
+      <Link href="/">
+        <a>{"<< Back to recipes list"}</a>
+      </Link>
+      <h2>{`Details page for ${id} `}</h2>
     </PageContainer>
   );
 }
