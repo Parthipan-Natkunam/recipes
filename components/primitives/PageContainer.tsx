8dc17586d08706d07bcf7a20d4b7170db2fd4ca1
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2021-03-29 23:56:57 +0530

    refactor recipe details page

--- components/primitives/PageContainer.tsx
+++ components/primitives/PageContainer.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 import styled from "styled-components";
-const PageContainer = styled.div`
+const StyledPageContainer = styled.div`
   padding: 1rem;
   background: ${(props) => props.bg ?? "#d3dce0"};
   min-height: 100vh;
 `;
+
+interface PageContainerProps {
+  children: React.ReactNode;
+  bg?: string;
+}
+
+const PageContainer: React.FC<PageContainerProps> = ({ children, bg }) => (
+  <StyledPageContainer bg={bg} data-testid="$recipe-app-pagecontainer">
+    {children}
+  </StyledPageContainer>
+);
 
 export default PageContainer;
--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
@@ -10,37 +11,59 @@ import { GET_RECIPE_BY_ID } from "../api/queries";
 export default function RecipeDetails({ recipe, error }) {
   const router = useRouter();
 
-  if (error) {
-    return (
-      <Error
-        title="Oops! Something Went Wrong"
-        description="Something went wrong. Please try reloading this page"
-        cta={{ text: "Reload page", action: router.reload }}
-      />
-    );
-  }
-
-  if (recipe === null) {
-    return (
-      <Error
-        title="Recipe Does Not Exist"
-        description="Unable to load data for this recipe. Please go back to the recipe list"
-        cta={{
-          text: "Back to Recipe List",
-          action: () => router.push("/", "/"),
-        }}
-      />
-    );
-  }
-
-  return (
-    <PageContainer bg="#fff">
+  const HeadAttributes = React.useMemo(
+    () => (
       <Head>
         <title>Recipe Detail</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <BackLink link="/" as="/" text="<< Back to recipes list" />
-      <RecipeDetail {...recipe} />
+    ),
+    []
+  );
+
+  const componentSelector = React.useMemo(() => {
+    return {
+      errorComponent: (
+        <Error
+          title="Oops! Something Went Wrong"
+          description="Something went wrong. Please try reloading this page"
+          cta={{ text: "Reload page", action: router.reload }}
+        />
+      ),
+      noDataComponent: (
+        <Error
+          title="Recipe Does Not Exist"
+          description="Unable to load data for this recipe. Please go back to the recipe list"
+          cta={{
+            text: "Back to Recipe List",
+            action: () => router.push("/", "/"),
+          }}
+        />
+      ),
+      detailComponent: (
+        <>
+          <BackLink link="/" as="/" text="<< Back to recipes list" />
+          <RecipeDetail {...recipe} />
+        </>
+      ),
+    };
+  }, []);
+
+  const innerComponent = React.useMemo(() => {
+    switch (true) {
+      case error:
+        return "errorComponent";
+      case recipe === null:
+        return "noDataComponent";
+      default:
+        return "detailComponent";
+    }
+  }, [recipe, error]);
+
+  return (
+    <PageContainer bg="#fff">
+      {HeadAttributes}
+      {componentSelector[innerComponent]}
     </PageContainer>
   );
 }
